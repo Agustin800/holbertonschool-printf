cc56a11afdc382c4a6597446a413635e88d9d5e6
Agustin800 <aguslahalo@gmail.com>
2025-03-25 15:13:54 +0000

    preparation of printf

---
+++ printf-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * _printf - do things
+ * @format: format
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
